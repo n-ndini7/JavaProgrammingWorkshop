d242d4ae43b1fb33eb5dc50d670768fa707c05f9
n-ndini7 <hp@RJs-PC>
2020-10-07 20:02:45 +0530

    [nandini] added UC7 - winning position

--- src/com/example/TicTacToeGame.java
+++ src/com/example/TicTacToeGame.java
@@ -9,6 +9,7 @@ public class TicTacToeGame {
 	public static char letter;
 	public static char[] board = new char[10];
 	public static int count = 0;
+	public static int turn;
 
 	public static char[] createBoard() {
 		for (int i = 0; i < board.length; i++)
@@ -59,20 +60,19 @@ public class TicTacToeGame {
 			compPos(c);
 	}
 
-	public static void desiredLocation() {
+	public static void desiredLocation(int t) {
 		// char turn = 'X';
 		char flag;
 		System.out.println("Do you wish to play ? (y/n)");
 		flag = sc.next().charAt(0);
+		System.out.println("Enter the letter:");
+		letter = sc.next().charAt(0);
 		while (flag == 'y' || flag == 'Y') {
 			System.out.println();
 			System.out.println("Enter the desired position :");
 			int choice = Integer.parseInt(sc.next());
 			if (choice >= 1 && choice <= 9) {
 				if (board[choice] == ' ') {
-
-					System.out.println("Enter the letter:");
-					letter = sc.next().charAt(0);
 					player = letter;
 					board[choice] = player;
 					count++;
@@ -92,29 +92,70 @@ public class TicTacToeGame {
 				System.out.println("Invalid position!!");
 			}
 			// printBoard();
-			System.out.println("Do you wish to continue ? (y/n)");
-			flag = sc.next().charAt(0);
-			// sc.nextLine();
+			if (count < 9) {
+				System.out.println("Do you wish to continue ? (y/n)");
+				flag = sc.next().charAt(0);
+			} else {
+				flag = 'n';
+			}
 		}
+		int dec = Winner();
+		if (dec == 1 && t == 1)
+			System.out.println("Player won the game!");
+		else if (dec == 1 && t == 2)
+			System.out.println("Computer won the game!");
+		else
+			System.out.println("It's a draw!");
+		System.out.println("Thankyou for playing!");
 	}
 
 	// uc4 and uc5 to check for empty block
 
+	public static int Winner() {
+		int i = 0;
+			if ((board[1] == 'X' && board[2] == 'X' && board[3] == 'X')
+					|| (board[4] == 'X' && board[5] == 'X' && board[6] == 'X')
+					|| (board[7] == 'X' && board[8] == 'X' && board[9] == 'X'))
+				i = 1;
+			if ((board[1] == 'X' && board[4] == 'X' && board[7] == 'X')
+					|| (board[2] == 'X' && board[5] == 'X' && board[8] == 'X')
+					|| (board[3] == 'X' && board[6] == 'X' && board[9] == 'X'))
+				i = 1;
+			else if ((board[1] == '0' && board[4] == '0' && board[7] == '0')
+					|| (board[2] == '0' && board[5] == '0' && board[8] == '0')
+					|| (board[3] == '0' && board[6] == '0' && board[9] == '0'))
+				i = 1;
+			else if ((board[1] == '0' && board[4] == '0' && board[7] == '0')
+					|| (board[2] == '0' && board[5] == '0' && board[8] == '0')
+					|| (board[3] == '0' && board[6] == '0' && board[9] == '0'))
+				i = 1;
+			else if ((board[1] == 'X' && board[5] == 'X' && board[9] == 'X')
+					|| (board[3] == 'X' && board[5] == 'X' && board[7] == 'X'))
+				i = 1;
+			else if ((board[1] == '0' && board[5] == '0' && board[9] == '0')
+					|| (board[3] == '0' && board[5] == '0' && board[7] == '0'))
+				i = 1;
+			else
+				i = 2;
+		return i;
+	}
+
 	public static void toss() {
 		System.out.println("Toss and play!");
-		int turn = ((int) Math.floor(Math.random() * 10) % 2) + 1;
+		turn = ((int) Math.floor(Math.random() * 10) % 2) + 1;
 		int choice = 1;
 		if (turn == 1) {
 			System.out.println("Player's turn!");
-			desiredLocation();
+			desiredLocation(turn);
 		} else {
 			System.out.println("Computer's turn!");
 			int l = ((int) Math.floor(Math.random() * 10) % 9) + 1;
 			board[l] = 'X';
 			System.out.println("Computer placed X at " + l + " position");
 			count++;
-			desiredLocation();
+			desiredLocation(turn);
 		}
+
 	}
 
 	public static void main(String[] args) {
@@ -126,6 +167,7 @@ public class TicTacToeGame {
 		// t.desiredLocation();
 		// t.printBoard();
 		t.toss();
+
 	}
 
 }
